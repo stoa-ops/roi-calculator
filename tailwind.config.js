4c1e9a12bb7929f32d8a922613ae8fac9a31d31d
stoa-ops <operations@stoa.agency>
2025-02-16 00:35:02 -0500

    fix: update CSS configuration for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -41,5 +40,3 @@ const config: Config = {
   },
   plugins: [require("@tailwindcss/forms")],
 };
-
-export default config;
